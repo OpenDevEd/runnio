0e3d78685feffd7fd910b43f383241dec97b2fba
echaoubi <abdochaoubi2000@gmail.com>
2023-08-31 10:59:40 +0100

    make the report list

--- report.js
+++ report.js
@@ -8,6 +8,56 @@ program
 */
 const axios = require('axios');
 
+
+
+
+async function fetchPeoplePage(params, apiKey) {
+    const baseUrl = 'https://app.runn.io/api/v0';
+
+    try {
+        const response = await axios.get(`${baseUrl}/people`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+            params: params,
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+async function fetchPeople(apiKey, options) {
+    let currentPage = 1;
+    const params = {};
+    const fetchedPeople = [];
+    let dateOfRun = new Date();
+    const fetchNextPage = async () => {
+        params.page = currentPage;
+        try {
+            // console.log("currentPage",currentPage);
+            const response = await fetchPeoplePage(params, apiKey);
+            fetchedPeople.push(...response);
+            if (response.length > 0) {
+                currentPage++;
+                if (currentPage % 120 == 0) {
+                    const SecondToComplteMinute = 60000 - (new Date().getTime() / 1000 - dateOfRun.getTime() / 1000);
+                    if (SecondToComplteMinute > 0) {
+                        await new Promise(resolve => setTimeout(resolve, SecondToComplteMinute));
+                    }
+                    dateOfRun = new Date();
+                }
+                await fetchNextPage();
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    };
+    await fetchNextPage();
+    return fetchedPeople;
+}
+
+
+
 async function fetchpeopleById(apiKey, params, id) {
     const baseUrl = `https://app.runn.io/api/v0/`;
     try {
@@ -38,8 +88,7 @@ const day = parseInt(stringg.substring(6, 8));
 return new Date(year, month, day);
 }
 
-async function report(id, options, config) {
-    const _id ="nr9hz84l8"; // you need give your own id , i am still working on it.
+async function reportOneByOne(options, config,userId,userName) {
     const today = new Date();
     let todayDay = today.getDay();
     let start = new Date(today.getFullYear(), today.getMonth(), today.getDate() - todayDay + 1 - 7);
@@ -49,7 +98,7 @@ async function report(id, options, config) {
         end: formatDate(end),
         include_assignments: true,
     };
-    const assignments = await fetchpeopleById(config.apikey, params, _id);
+    const assignments = await fetchpeopleById(config.apikey, params, userId);
     // console.log(assignments.assignments);
     let totalHours = 0;
     for (let i = 0; i < assignments.assignments.length; i++) {
@@ -63,13 +112,27 @@ async function report(id, options, config) {
         }
         let daysInAssignment = (endAssignment - startAssignment) / 1000 / 60 / 60 / 24 + 1;
         totalHours += assignments.assignments[i].minutes_per_day*daysInAssignment;
-
     }
-    console.log("Total hours:", totalHours / 60);
-    console.log("Last Monday:", formatDate(start));
-    console.log("Last Friday:", formatDate(end));
-
+    return  totalHours / 60 ;
 }
 
+
+
+
+
+
+
+
+async function report(id, options, config) {
+    const people = await fetchPeople(config.apikey, options);
+    const report = {};
+    for (let i=0;i<people.length;i++)
+    {
+        const hours=await reportOneByOne(options, config,people[i].id,people[i].name)
+        report[people[i].name] = hours;
+
+    }
+    console.log(report)
+}
 
 module.exports = report;
